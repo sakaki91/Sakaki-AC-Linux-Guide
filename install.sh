eefba000d8878fc6cbf44e6ad61bc371ef67821c
sakaki91 <sakaki@tuta.io>
2026-05-31 03:38:17 -0300

    Update - Flow & Typo Fix

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# @sakaki91 on GitHub!
 
-modBaseDownload(){
-    mkdir -p "$GAMEPATH"/{Temp,Backup} && cp "$GAMEPATH"/AssettoCorsa "$GAMEPATH"/Backup/AssettoCorsaBackup.exe
+CMBaseDownload(){
+    mkdir -p "$GAMEPATH"/temp
+    [[ ! -d "$GAMEPATH"/backup ]] && mkdir "$GAMEPATH"/backup
+    [[ ! -f "$GAMEPATH"/backup/AssettoCorsaBackup.exe ]] && cp "$GAMEPATH"/AssettoCorsa.exe "$GAMEPATH"/backup/AssettoCorsaBackup.exe
     [[ ! -f "$GAMEPATH"/AssettoCorsa_original.exe ]] && cp "$GAMEPATH"/AssettoCorsa.exe "$GAMEPATH"/AssettoCorsa_original.exe
     (
-        cd "$GAMEPATH"/Temp
+        cd "$GAMEPATH"/temp
         wget -c https://acstuff.ru/app/latest.zip
         wget -c https://acstuff.club/u/blob/ac-fonts.zip
         unzip latest.zip
+	unzip ac-fonts.zip
         [[ ! -d "$GAMEPATH"/content/fonts ]] && mkdir -p "$GAMEPATH"/content/fonts
-        unzip ac-fonts.zip
         mv 'Content Manager.exe' "$GAMEPATH"/AssettoCorsa.exe
         mv system "$GAMEPATH"/content/fonts/
     )
+    rm -rf "$GAMEPATH"/temp
     exit
 }
 
@@ -30,37 +34,40 @@ if [[ $1 == "--native" ]]; then
     GAMEPATH="$HOME"/.local/share/Steam/steamapps/common/assettocorsa
     GAMEPREFIX="$HOME"/.local/share/Steam/steamapps/compatdata/244210/pfx
     dependencyInstall
-    modBaseDownload
+    CMBaseDownload
 elif [[ $1 == "--flatpak" ]]; then
     GAMEPATH="$HOME"/.var/app/com.valvesoftware.Steam/data/Steam/steamapps/common/assettocorsa
     GAMEPREFIX="$HOME"/.var/app/com.valvesoftware.Steam/data/Steam/steamapps/compatdata/244210/pfx
     dependencyInstall
-    modBaseDownload
+    CMBaseDownload
 elif [[ $1 == "--custom" ]]; then
     GAMEPATH=$(zenity --file-selection --directory --title "Select the game folder.")
     GAMEPREFIX=$(zenity --file-selection --directory --title "Select the game's prefix folder (244210/pfx only).")
+    if [[ -z $GAMEPATH || -z $GAMEPREFIX ]]; then
+	    clear
+	    printf "\e[0;91mERROR:\e[0m empty value.\n"
+	    exit
+    fi
     clear
     while true; do
         printf "GAME PATH: $GAMEPATH\nGAME PREFIX: $GAMEPREFIX\n\n"
         read -p "Do you want to use these directories? [Y/n] " customDirConfirmation
         [[ -z $customDirConfirmation ]] && customDirConfirmation="y"
         if [[ $customDirConfirmation == "Y" || $customDirConfirmation == "y" ]]; then
-            break
+	    dependencyInstall
+	    CMBasedDownload
         elif [[ $customDirConfirmation == "N" || $customDirConfirmation == "n" ]]; then
             break 2
         else
-            printf "ERROR: Invalid option\n"
+            printf "\e[0;91mERROR:\e[0m Invalid option.\n"
             sleep 2
             clear
         fi
     done
-    dependencyInstall
-    modBaseDownload
 else
-    cat .logo
-    printf "\nERROR: Invalid option\n\nExamples:\n\n"
-    printf "%-30s %0s\n" "./install --native" "Configures the locally installed game to be in the native Steam folder."
-    printf "%-30s %0s\n" "./install --flatpak" "Configures the locally installed game to be in the flatpak Steam folder."
-    printf "%-30s %0s\n\n" "./install --custom" "Configure the installed game to a different disk/partition."
-	exit
+    printf "\e[0;91mERROR:\e[0m Invalid option.\n\nExamples:\n"
+    printf "%-25s %0s\n" "./install --native" "Configures the locally installed game to be in the native Steam folder."
+    printf "%-25s %0s\n" "./install --flatpak" "Configures the locally installed game to be in the flatpak Steam folder."
+    printf "%-25s %0s\n\n" "./install --custom" "Configure the installed game to a different disk/partition."
+    exit
 fi
